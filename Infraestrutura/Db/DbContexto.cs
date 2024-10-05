1cf3523c8e07f2faea62e5647910ef169289318b
Luiz Fernando Rodrigues <luizfr.rodrigues@gmail.com>
2024-10-05 11:06:52 -0300

    Add migration seed administrador

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -18,6 +18,18 @@ namespace veiculos_minimal_api.Infraestrutura.Db
 
         public DbSet<Administrador> Administradores { get; set; } = default!;
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Administrador>().HasData(
+                new Administrador {
+                    Id = 1,
+                    Email = "administrador@teste.com",
+                    Senha = "123456",
+                    Perfil = "Adm"
+                }
+            );
+        }
+        
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if(!optionsBuilder.IsConfigured)
